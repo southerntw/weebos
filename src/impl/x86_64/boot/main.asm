68bc51720b22848b4f4785ef74a1e719546cb8e9
southerntw <fauzanmansur.a@gmail.com>
2022-10-10 17:06:30 +0000

    recommit long mode

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -7,16 +7,16 @@ start:
 	mov esp, stack_top
 
 	call check_multiboot
-	call check_cpuid ; check cpu support
+	call check_cpuid
 	call check_long_mode
 
 	call setup_page_tables
 	call enable_paging
 
- 	lgdt [gdt64.pointer]
- 	jmp gdt64.code_segment:long_mode_start
+	lgdt [gdt64.pointer]
+	jmp gdt64.code_segment:long_mode_start
 
-	hlt	
+	hlt
 
 check_multiboot:
 	cmp eax, 0x36d76289
@@ -54,7 +54,7 @@ check_long_mode:
 	cpuid
 	test edx, 1 << 29
 	jz .no_long_mode
-
+	
 	ret
 .no_long_mode:
 	mov al, "L"
@@ -64,7 +64,7 @@ setup_page_tables:
 	mov eax, page_table_l3
 	or eax, 0b11 ; present, writable
 	mov [page_table_l4], eax
-
+	
 	mov eax, page_table_l2
 	or eax, 0b11 ; present, writable
 	mov [page_table_l3], eax
@@ -74,12 +74,12 @@ setup_page_tables:
 
 	mov eax, 0x200000 ; 2MiB
 	mul ecx
-	or eax, 0b10000011 ; present, writable, huge page file
+	or eax, 0b10000011 ; present, writable, huge page
 	mov [page_table_l2 + ecx * 8], eax
 
 	inc ecx ; increment counter
 	cmp ecx, 512 ; checks if the whole table is mapped
-	jne .loop ; if not continue to loop
+	jne .loop ; if not, continue
 
 	ret
 
@@ -88,11 +88,11 @@ enable_paging:
 	mov eax, page_table_l4
 	mov cr3, eax
 
-	; enable physical adress extension (PAE)
+	; enable PAE
 	mov eax, cr4
 	or eax, 1 << 5
 	mov cr4, eax
-	
+
 	; enable long mode
 	mov ecx, 0xC0000080
 	rdmsr
@@ -111,7 +111,7 @@ error:
 	mov dword [0xb8000], 0x4f524f45
 	mov dword [0xb8004], 0x4f3a4f52
 	mov dword [0xb8008], 0x4f204f20
-	mov byte [0xb800a], al
+	mov byte  [0xb800a], al
 	hlt
 
 section .bss
@@ -121,9 +121,9 @@ page_table_l4:
 page_table_l3:
 	resb 4096
 page_table_l2:
-	resb 4095
+	resb 4096
 stack_bottom:
-	resb 4096 * 4			
+	resb 4096 * 4
 stack_top:
 
 section .rodata
